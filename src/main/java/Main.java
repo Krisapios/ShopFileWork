a2ef921d6578a0daebe20dc58432b560cac60b6b
Godrik <krisapios@gmail.com>
2022-10-30 20:33:37 +0600

    +shop.xml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,65 @@
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
 import java.io.File;
 import java.io.IOException;
-import java.io.ObjectInputFilter;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, Exception {
         Scanner scanner = new Scanner(System.in);
         String[] products = {"Хлеб", "Яблоки", "Молоко"};
         int[] prices = {100, 200, 300};
-
         Basket basket = new Basket(products, prices);
         File basketJson = new File("basket.json");
-        ClientLog log = new ClientLog();
+        File basketText = new File("basket.txt");
+        File csvFile = new File("client.csv");
+        ClientLog clientLog = new ClientLog();
 
-        if (!basketJson.createNewFile()) {
-            basket = Basket.loadJson(basketJson);
-            System.out.println("Список загружен.");
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File("shop.xml"));
+
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        String loadFileName = xPath
+                .compile("config/load/fileName")
+                .evaluate(doc);
+        boolean loadFileEnabled = Boolean.parseBoolean(xPath
+                .compile("config/load/enabled")
+                .evaluate(doc));
+        String loadFileFormat = xPath
+                .compile("config/load/format")
+                .evaluate(doc);
+        boolean saveFileEnabled = Boolean.parseBoolean(xPath
+                .compile("config/save/enabled")
+                .evaluate(doc));
+        String saveFileName = xPath
+                .compile("config/save/fileName")
+                .evaluate(doc);
+        String saveFileFormat = xPath
+                .compile("config/load/format")
+                .evaluate(doc);
+        boolean logFileEnabled = Boolean.parseBoolean(xPath
+                .compile("config/log/enabled")
+                .evaluate(doc));
+        String logFileName = xPath
+                .compile("config/log/fileName")
+                .evaluate(doc);
+
+        if (loadFileEnabled && loadFileFormat.equals("json")) {
+            if (!basketJson.createNewFile()) {
+                basket = Basket.loadJson(new File(loadFileName));
+                System.out.println("Список загружен.");
+            } else if (loadFileEnabled && loadFileFormat.equals("txt")) {
+                if (!basketText.createNewFile()) {
+                    basket = Basket.loadFromTxtFile(new File(loadFileName));
+                    System.out.println("Список загружен.");
+                }
+            }
         }
-
         System.out.println("Список возможных товаров для покупки");
         for (int i = 0; i < products.length; i++) {
             System.out.println((i + 1) + ". " + products[i] + " " + prices[i]);
@@ -31,12 +73,17 @@ public class Main {
             String[] parts = input.split(" ");
             int productNumber = Integer.parseInt(parts[0]) - 1;
             int productCount = Integer.parseInt(parts[1]);
-
             basket.addToCart(productNumber, productCount);
-            log.log(productNumber, productCount); //записываем выбор в журнал
-            log.exportAsCSV(new File("log.csv")); //выгружаем журнал в csv
+            clientLog.log(productNumber, productCount);
         }
-        basket.saveJson(basketJson);
+        if (saveFileEnabled && saveFileFormat.equals("json")) {
+            basket.saveJson(new File(saveFileName));
+        } else if (saveFileEnabled && saveFileFormat.equals("txt")) {
+            basket.saveTxt(new File(saveFileName));
+        }
+        if (logFileEnabled) {
+            clientLog.exportAsCSV(new File(logFileName));
+        }
         basket.printCart();
     }
 }
